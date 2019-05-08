84a9d38c56ad894af220ada46ef4711b0e66ed05
Richard Cho <richardcho100@gmail.com>
2019-05-08 05:08:07 +0000

    Done.

--- config/environment.rb
+++ config/environment.rb
@@ -2,6 +2,8 @@ ENV['SINATRA_ENV'] ||= "development"
 ENV['RACK_ENV'] ||= "development"
 
 require 'bundler/setup'
+require 'date'
+require "pry"
 Bundler.require(:default, ENV['SINATRA_ENV'])
 
 require './app'
